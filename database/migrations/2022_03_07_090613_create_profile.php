0c11cdd2cb2a84d4a5c566bc55bb11cc3a54c8c7
mabrursatori <mabrursatori@gmail.com>
2022-03-07 16:50:20 +0700

    Create Table With Migration

---
+++ database/migrations/2022_03_07_090613_create_profile.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->longText('logo');
+            $table->longText('registration');
+            $table->string('title');
+            $table->longText('image');
+            $table->longText('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
