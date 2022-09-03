927af12c75bb631c89a47e0e098744a29555094e
royhan76 <beensholih@gmail.com>
2022-09-03 22:15:52 +0700

    path css hal depan

--- resources/views/layouts/home/main.blade.php
+++ resources/views/layouts/home/main.blade.php
@@ -19,18 +19,27 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    {{-- <link href="/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
     <link href="/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
     <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
     <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
     <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet"> --}}
+
+    <link href={{ asset('/assets/vendor/fontawesome-free/css/all.min.css') }} rel="stylesheet">
+    <link href={{ asset('/assets/vendor/animate.css/animate.min.css') }} rel="stylesheet">
+    <link href={{ asset('/assets/vendor/bootstrap/css/bootstrap.min.css') }} rel="stylesheet">
+    <link href={{ asset('/assets/vendor/bootstrap-icons/bootstrap-icons.css') }} rel="stylesheet">
+    <link href={{ asset('/assets/vendor/boxicons/css/boxicons.min.css') }} rel="stylesheet">
+    <link href={{ asset('/assets/vendor/glightbox/css/glightbox.min.css') }} rel="stylesheet">
+    <link href={{ asset('/assets/vendor/remixicon/remixicon.css') }} rel="stylesheet">
+    <link href={{ asset('/assets/vendor/swiper/swiper-bundle.min.css') }} rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="/assets/css/style.css" rel="stylesheet">
-    <link href="/assets/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href='{{ asset('/assets/css/style.css') }}'>
+    <link href='{{ asset('/atlantis/css/atlantis.css" rel="stylesheet') }}'>
 
     <!-- =======================================================
   * Template Name: Medilab - v4.7.1
